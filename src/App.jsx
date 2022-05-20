572ea4d783c619879b7182f644a2b35411757408
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-20 16:22:20 -0600

    Added githubbutton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Landing from './components/Landing'
+import GitHubButton from './components/GitHubButton'
 
 function App() {
     return (
         <div className="app-container">
+            <GitHubButton />
             <Landing />
         </div>
     )
