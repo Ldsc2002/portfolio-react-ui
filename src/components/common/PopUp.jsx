8ad60d1b744c9535267a35ccaafa8e799ba2c330
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-21 17:08:51 -0600

    Styled pop up

--- src/components/common/PopUp.jsx
+++ src/components/common/PopUp.jsx
@@ -6,8 +6,12 @@ function PopUp({ trigger, children, setTrigger }) {
     return (trigger) ? (
         <div className={classes.popUp}>
             <div className={classes.card}>
-                <button type="button" onClick={() => setTrigger(false)}>x</button>
-                {children}
+                <div className={classes.topBar}>
+                    <span className={classes.button} onClick={() => setTrigger(false)} />
+                </div>
+                <div className={classes.content}>
+                    {children}
+                </div>
             </div>
         </div>
     )
