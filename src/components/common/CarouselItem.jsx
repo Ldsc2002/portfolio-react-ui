fa6efd8e79f948590ba09de5f6277ae4f8d3459f
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-22 15:24:19 -0600

    WIP display more info

--- src/components/common/CarouselItem.jsx
+++ src/components/common/CarouselItem.jsx
@@ -5,7 +5,7 @@ import btnImage from '../../images/OpenIcon.svg'
 
 function CarouselItem({ data, action }) {
     const popUpContent = (
-        <div>{data.content.title}</div>
+        <div>{data.content.url}</div>
     )
 
     return (
@@ -27,13 +27,22 @@ function CarouselItem({ data, action }) {
 
             <div className={classes.container}>
                 <img className={classes.image} alt={data.content.title} src={`/${data.content.image}`} />
+                <div className={classes.shortDesc}>{data.content.desc}</div>
             </div>
         </div>
     )
 }
 
 CarouselItem.propTypes = {
-    data: PropTypes.any,
+    data: PropTypes.shape({
+        id: PropTypes.number,
+        content: PropTypes.shape({
+            title: PropTypes.string,
+            image: PropTypes.string,
+            desc: PropTypes.string,
+            url: PropTypes.string,
+        }),
+    }),
     action: PropTypes.func,
 }
 
