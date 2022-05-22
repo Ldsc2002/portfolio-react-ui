6a6d5cf632095403da5b2bbd559e574fbebf84fd
Ldsc2002 <luissantoscuellar@gmail.com>
2022-05-22 00:33:37 -0600

    Updated UI

--- src/components/common/PopUp.jsx
+++ src/components/common/PopUp.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classes from '../../style/PopUp.module.css'
+import btnImage from '../../images/CloseIcon.svg'
 
 function PopUp({ trigger, children, setTrigger }) {
     return (trigger) ? (
         <div className={classes.popUp}>
             <div className={classes.card}>
                 <div className={classes.topBar}>
-                    <span
-                        aria-label="Close pop-up"
-                        role="button"
-                        className={classes.button}
-                        tabIndex={0}
-                        onKeyDown={() => setTrigger(false)}
-                        onClick={() => setTrigger(false)}
-                    />
+                    <button className={classes.btnContainer} type="button" onKeyDown={() => setTrigger(false)} onClick={() => setTrigger(false)}>
+                        <img alt="" src={btnImage} className={classes.button} />
+                    </button>
                 </div>
                 <div className={classes.content}>
                     {children}
